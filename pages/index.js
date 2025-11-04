ad2f9d028c2cfe844fc53eb5a1b733017814a293
Rafael Azevedo <rflazv@Outlook.com>
2025-11-04 18:53:58 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Eu não sei porque você está aqui, mas pelo visto..você é curioso. 😜</h1>
+  return (
+    <h1>
+      Eu não sei porque você está aqui, mas pelo visto..você é curioso. 😜
+    </h1>
+  );
 }
 
 export default Home;
